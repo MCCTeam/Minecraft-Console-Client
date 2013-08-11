8e77fc6bdd907427f89896498c801882b362b4a9
Allu2 <aleksi.ajp@gmail.com>
2013-08-11 22:17:01 +0300

    Replaced stuff requiring Bouncy Castle so it isn't required anymore.
    Replaced stuff requiring Bouncy Castle so it isn't required anymore. I used methods available from libraries already imported to generate the key.

--- MinecraftClient/Crypto.cs
+++ MinecraftClient/Crypto.cs
@@ -7,8 +7,7 @@ using java.security;
 using java.security.spec;
 using javax.crypto;
 using javax.crypto.spec;
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Parameters;
+
 
 namespace MinecraftClient
 {
@@ -24,12 +23,13 @@ namespace MinecraftClient
             KeyFactory localKeyFactory = KeyFactory.getInstance("RSA");
             return localKeyFactory.generatePublic(localX509EncodedKeySpec);
         }
-
         public static SecretKey GenerateAESPrivateKey()
         {
-            CipherKeyGenerator var0 = new CipherKeyGenerator();
-            var0.Init(new KeyGenerationParameters(new Org.BouncyCastle.Security.SecureRandom(), 128));
-            return new SecretKeySpec(var0.GenerateKey(), "AES");
+    	    AesManaged aes = new AesManaged();
+			aes.KeySize = 128;
+			aes.GenerateKey();
+			byte[] key = aes.Key;
+            return new SecretKeySpec(key, "AES");
         }
 
         public static byte[] getServerHash(String toencode, PublicKey par1PublicKey, SecretKey par2SecretKey)
