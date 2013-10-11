aafd825aa884875b412d7385902bc92a4e5f0022
justcool393 <wayhay53@gmail.com>
2013-10-10 21:17:30 -0700

    Update supported versions
    Change from 1.6.2 to 1.6.4 in the first output line.

--- MinecraftClient/Program.cs
+++ MinecraftClient/Program.cs
@@ -23,7 +23,7 @@ namespace MinecraftClient
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Console Client for MC 1.4.6 to 1.6.2 - v" + Version + " - By ORelio (or3L1o@live.fr)");
+            Console.WriteLine("Console Client for MC 1.4.6 to 1.6.4 - v" + Version + " - By ORelio (or3L1o@live.fr)");
 
             //Basic Input/Output ?
             if (args.Length >= 1 && args[args.Length - 1] == "BasicIO")
