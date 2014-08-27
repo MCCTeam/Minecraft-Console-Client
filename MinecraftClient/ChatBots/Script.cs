dea2862018bba17f785311755c9d5c6f18e4cd89
ORelio <oreliogitantispam.l0gin@spamgourmet.com>
2014-08-27 17:22:27 +0200

    Fix "log" in scripts displaying the comment twice
    Bug report by TorchRJ_

--- MinecraftClient/ChatBots/Script.cs
+++ MinecraftClient/ChatBots/Script.cs
@@ -106,7 +106,7 @@ namespace MinecraftClient.ChatBots
                                     {
                                         sleepticks = 0; Update(); //Unknown command : process next line immediately
                                     }
-                                    else LogToConsole(instruction_line);
+                                    else if (instruction_name.ToLower() != "log") { LogToConsole(instruction_line); }
                                     break;
                             }
                         }
