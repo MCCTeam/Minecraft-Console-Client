693073edfcf532ed7920c7e3e8a732897a073381
ORelio <oreliogitantispam.l0gin@spamgourmet.com>
2017-03-11 15:28:32 +0100

    Replace DnDns with HeijdenDns
    HeijdenDns seems to do a better job at querying SRV records

---
+++ MinecraftClient/Protocol/Dns/Records/RecordA.cs
@@ -0,0 +1,38 @@
+using System;
+/*
+ 3.4.1. A RDATA format
+
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ADDRESS                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+where:
+
+ADDRESS         A 32 bit Internet address.
+
+Hosts that have multiple Internet addresses will have multiple A
+records.
+ * 
+ */
+namespace Heijden.DNS
+{
+	public class RecordA : Record
+	{
+		public System.Net.IPAddress Address;
+
+		public RecordA(RecordReader rr)
+		{
+			System.Net.IPAddress.TryParse(string.Format("{0}.{1}.{2}.{3}",
+				rr.ReadByte(),
+				rr.ReadByte(),
+				rr.ReadByte(),
+				rr.ReadByte()), out this.Address);
+		}
+
+		public override string ToString()
+		{
+			return Address.ToString();
+		}
+
+	}
+}
