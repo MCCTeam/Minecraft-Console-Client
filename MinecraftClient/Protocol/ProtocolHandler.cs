da8dd9b8fe057994a9f5a28df916e5e06d0762a9
LinTx <lintx@lintx.org>
2018-10-28 21:20:28 +0800

    general 1.13.2 support(v404 protocol)

--- MinecraftClient/Protocol/ProtocolHandler.cs
+++ MinecraftClient/Protocol/ProtocolHandler.cs
@@ -106,7 +106,7 @@ namespace MinecraftClient.Protocol
             int[] supportedVersions_Protocol16 = { 51, 60, 61, 72, 73, 74, 78 };
             if (Array.IndexOf(supportedVersions_Protocol16, ProtocolVersion) > -1)
                 return new Protocol16Handler(Client, ProtocolVersion, Handler);
-            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401 };
+            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401 , 404 };
             if (Array.IndexOf(supportedVersions_Protocol18, ProtocolVersion) > -1)
                 return new Protocol18Handler(Client, ProtocolVersion, Handler, forgeInfo);
             throw new NotSupportedException("The protocol version no." + ProtocolVersion + " is not supported.");
@@ -193,6 +193,8 @@ namespace MinecraftClient.Protocol
                         return 393;
                     case "1.13.1":
                         return 401;
+                    case "1.13.2":
+                        return 404;
                     default:
                         return 0;
                 }
