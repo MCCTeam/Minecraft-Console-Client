6ad8a3ca699cd86fa781c737a0fa015138d55532
Sheldon Rupp <me@suce.dev>
2019-07-02 18:35:01 +0200

    Add 1.14.3 support

--- MinecraftClient/Protocol/ProtocolHandler.cs
+++ MinecraftClient/Protocol/ProtocolHandler.cs
@@ -111,7 +111,7 @@ namespace MinecraftClient.Protocol
             int[] supportedVersions_Protocol16 = { 51, 60, 61, 72, 73, 74, 78 };
             if (Array.IndexOf(supportedVersions_Protocol16, ProtocolVersion) > -1)
                 return new Protocol16Handler(Client, ProtocolVersion, Handler);
-            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401, 404, 477, 480, 485 };
+            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401, 404, 477, 480, 485, 490 };
             if (Array.IndexOf(supportedVersions_Protocol18, ProtocolVersion) > -1)
                 return new Protocol18Handler(Client, ProtocolVersion, Handler, forgeInfo);
             throw new NotSupportedException("The protocol version no." + ProtocolVersion + " is not supported.");
@@ -207,6 +207,8 @@ namespace MinecraftClient.Protocol
                         return 480;
                     case "1.14.2":
                         return 485;
+                    case "1.14.3":
+                        return 490;
                     default:
                         return 0;
                 }
