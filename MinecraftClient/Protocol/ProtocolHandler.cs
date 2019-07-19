10790f2982b2526b046442de49f29f23f380cfa1
0Jumpero <53096124+0Jumpero@users.noreply.github.com>
2019-07-19 21:55:06 +0200

    Changed the protocol version
    I got the wrong protocol version previously. Got the right one from https://wiki.vg/Protocol_version_numbers for the live version on Minecraft

--- MinecraftClient/Protocol/ProtocolHandler.cs
+++ MinecraftClient/Protocol/ProtocolHandler.cs
@@ -111,7 +111,7 @@ namespace MinecraftClient.Protocol
             int[] supportedVersions_Protocol16 = { 51, 60, 61, 72, 73, 74, 78 };
             if (Array.IndexOf(supportedVersions_Protocol16, ProtocolVersion) > -1)
                 return new Protocol16Handler(Client, ProtocolVersion, Handler);
-            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401, 404, 477, 480, 485, 490, 494 };
+            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401, 404, 477, 480, 485, 490, 498 };
             if (Array.IndexOf(supportedVersions_Protocol18, ProtocolVersion) > -1)
                 return new Protocol18Handler(Client, ProtocolVersion, Handler, forgeInfo);
             throw new NotSupportedException("The protocol version no." + ProtocolVersion + " is not supported.");
@@ -210,7 +210,7 @@ namespace MinecraftClient.Protocol
                     case "1.14.3":
                         return 490;
                     case "1.14.4":
-                        return 494;
+                        return 498;
                     default:
                         return 0;
                 }
