MCCTeam/Minecraft-Console-Client

commit aaf1e8311bd672354b71fbad8a896475b56e89ca

https://github.com/ORelio/Minecraft-Console-Client/issues/427: TriggerOnLogin doesn't work when /reco

MinecraftClient/McTcpClient.cs
@@ -354,6 +354,9 @@ namespace MinecraftClient
         /// </summary>
         public void Disconnect()
         {
+            foreach (ChatBot bot in bots)
+                bot.OnDisconnect(ChatBot.DisconnectReason.ConnectionLost, "Disconnected");
+
             botsOnHold.Clear();
             botsOnHold.AddRange(bots);
 

commit 52d98538b3f1c06b634ab8a76dd7ec9abe0baac4

Fix AutoRelog running twice on disconnection
Bug introduced in commit aaf1e8311bd672354b71fbad8a896475b56e89ca
Implement User-Initiated logout reason to ignore the second logout event
See #817

MinecraftClient/ChatBot.cs
@@ -27,7 +27,7 @@ namespace MinecraftClient
     /// </summary>
     public abstract class ChatBot
     {
-        public enum DisconnectReason { InGameKick, LoginRejected, ConnectionLost };
+        public enum DisconnectReason { InGameKick, LoginRejected, ConnectionLost, UserLogout };
 
         //Handler will be automatically set on bot loading, don't worry about this
         public void SetHandler(McTcpClient handler) { this._handler = handler; }

MinecraftClient/ChatBots/AutoRelog.cs
@@ -58,29 +58,37 @@ namespace MinecraftClient.ChatBots
 
         public override bool OnDisconnect(DisconnectReason reason, string message)
         {
-            message = GetVerbatim(message);
-            string comp = message.ToLower();
-
-            if (Settings.DebugMessages)
-                LogToConsole("Got disconnected with message: " + message);
-
-            foreach (string msg in dictionary)
+            if (reason == DisconnectReason.UserLogout)
             {
-                if (comp.Contains(msg))
-                {
-                    if (Settings.DebugMessages)
-                        LogToConsole("Message contains '" + msg + "'. Reconnecting.");
-
-                    LogToConsole("Waiting " + delay + " seconds before reconnecting...");
-                    System.Threading.Thread.Sleep(delay * 1000);
-                    McTcpClient.ReconnectionAttemptsLeft = attempts;
-                    ReconnectToTheServer();
-                    return true;
-                }
+                if (Settings.DebugMessages)
+                    LogToConsole("Ignoring disconnection initiated by User or MCC bot");
             }
+            else
+            {
+                message = GetVerbatim(message);
+                string comp = message.ToLower();
 
-            if (Settings.DebugMessages)
-                LogToConsole("Message not containing any defined keywords. Ignoring.");
+                if (Settings.DebugMessages)
+                    LogToConsole("Got disconnected with message: " + message);
+
+                foreach (string msg in dictionary)
+                {
+                    if (comp.Contains(msg))
+                    {
+                        if (Settings.DebugMessages)
+                            LogToConsole("Message contains '" + msg + "'. Reconnecting.");
+
+                        LogToConsole("Waiting " + delay + " seconds before reconnecting...");
+                        System.Threading.Thread.Sleep(delay * 1000);
+                        McTcpClient.ReconnectionAttemptsLeft = attempts;
+                        ReconnectToTheServer();
+                        return true;
+                    }
+                }
+
+                if (Settings.DebugMessages)
+                    LogToConsole("Message not containing any defined keywords. Ignoring.");
+            }
 
             return false;
         }

MinecraftClient/McTcpClient.cs
@@ -349,12 +349,12 @@ namespace MinecraftClient
         }
 
         /// <summary>
-        /// Disconnect the client from the server
+        /// Disconnect the client from the server (initiated from MCC)
         /// </summary>
         public void Disconnect()
         {
             foreach (ChatBot bot in bots.ToArray())
-                bot.OnDisconnect(ChatBot.DisconnectReason.ConnectionLost, "Disconnected");
+                bot.OnDisconnect(ChatBot.DisconnectReason.UserLogout, "");
 
             botsOnHold.Clear();
             botsOnHold.AddRange(bots);
@@ -721,7 +721,7 @@ namespace MinecraftClient
         }
 
         /// <summary>
-        /// When connection has been lost
+        /// When connection has been lost, login was denied or played was kicked from the server
         /// </summary>
         public void OnConnectionLost(ChatBot.DisconnectReason reason, string message)
         {
@@ -751,6 +751,9 @@ namespace MinecraftClient
                     ConsoleIO.WriteLine("Login failed :");
                     ConsoleIO.WriteLineFormatted(message);
                     break;
+
+                case ChatBot.DisconnectReason.UserLogout:
+                    throw new InvalidOperationException("User-initiated logout should be done by calling Disconnect()");
             }
 
             foreach (ChatBot bot in bots.ToArray())