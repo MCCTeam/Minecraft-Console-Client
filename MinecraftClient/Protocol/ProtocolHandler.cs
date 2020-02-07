668c6cf6f792ee9abde591e64e56fa6704c19b7f
rvankammen <rvankammen@live.nl>
2020-02-07 17:22:55 +0100

    Added the 1.15.2 protocol

--- MinecraftClient/Protocol/ProtocolHandler.cs
+++ MinecraftClient/Protocol/ProtocolHandler.cs
@@ -111,7 +111,7 @@ namespace MinecraftClient.Protocol
             int[] supportedVersions_Protocol16 = { 51, 60, 61, 72, 73, 74, 78 };
             if (Array.IndexOf(supportedVersions_Protocol16, ProtocolVersion) > -1)
                 return new Protocol16Handler(Client, ProtocolVersion, Handler);
-            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401, 404, 477, 480, 485, 490, 498, 573, 575 };
+            int[] supportedVersions_Protocol18 = { 4, 5, 47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401, 404, 477, 480, 485, 490, 498, 573, 575, 578};
             if (Array.IndexOf(supportedVersions_Protocol18, ProtocolVersion) > -1)
                 return new Protocol18Handler(Client, ProtocolVersion, Handler, forgeInfo);
             throw new NotSupportedException("The protocol version no." + ProtocolVersion + " is not supported.");
@@ -216,6 +216,8 @@ namespace MinecraftClient.Protocol
                         return 573;
                     case "1.15.1":
                         return 575;
+                    case "1.15.2":
+                        return 578;
                     default:
                         return 0;
                 }
