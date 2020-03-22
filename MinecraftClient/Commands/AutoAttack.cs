850eae86d71360a64365316d670c0b771e90f5e8
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-03-22 16:04:59 +0800

    Add command to turn on and off auto attack

---
+++ MinecraftClient/Commands/AutoAttack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinecraftClient.Commands
+{
+    class AutoAttack : Command
+    {
+        public override string CMDName { get { return "autoattack"; } }
+        public override string CMDDesc { get { return "autoattack <on|off>: Enable/disable auto attack mobs."; } }
+
+        public override string Run(McTcpClient handler, string command)
+        {
+            if (hasArg(command))
+            {
+                string state = getArg(command);
+                if (state.ToLower() == "on")
+                {
+                    if (!handler.AutoAttack)
+                    {
+                        handler.AutoAttack = true;
+                        return "Auto attack turned on.";
+                    }
+                    else
+                    {
+                        return "Auto attack is on.";
+                    }
+                }else if (state.ToLower() == "off")
+                {
+                    if (handler.AutoAttack)
+                    {
+                        handler.AutoAttack = false;
+                        return "Auto attack turned off.";
+                    }
+                    else
+                    {
+                        return "Auto attack is off.";
+                    }
+                }
+                return "";
+            }
+            else return CMDDesc;
+        }
+    }
+}
