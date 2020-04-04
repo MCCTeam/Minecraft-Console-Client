addcf91d878380ae02e443f88ed3aa57938ad908
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-04-04 19:18:18 +0800

    Rename command Status to Health

--- MinecraftClient/Commands/Health.cs
+++ MinecraftClient/Commands/Health.cs
@@ -5,14 +5,14 @@ using System.Text;
 
 namespace MinecraftClient.Commands
 {
-    class Status : Command
+    class Health : Command
     {
         public override string CMDName { get { return "status"; } }
         public override string CMDDesc { get { return "status: Display Health and Food saturation."; } }
 
         public override string Run(McTcpClient handler, string command, Dictionary<string, object> localVars)
         {
-            return "Health: " + handler.GetHealth() + "\n[MCC] Saturation: " + handler.GetSaturation(); // any good ways to print multiple lines?
+            return "Health: " + handler.GetHealth() + ", Saturation: " + handler.GetSaturation();
         }
     }
 }
