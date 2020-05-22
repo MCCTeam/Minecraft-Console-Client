c0b7573ea7a441a871dd9ad67760b7ac2cf6b4be
Reinforce Zwei <shingleogchu@gmail.com>
2020-05-22 09:09:18 +0800

    Remove unused using

--- MinecraftClient/ChatBots/AutoFishing.cs
+++ MinecraftClient/ChatBots/AutoFishing.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using MinecraftClient.Mapping;
 using MinecraftClient.Inventory;
 
