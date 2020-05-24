d6022d1ee9fb7445535414f75e1837e98d1b9fb9
ReinforceZwei <shingleongchu@gmail.com>
2020-05-24 09:33:21 +0800

    Rename ClickWindow to WindowAction

---
+++ MinecraftClient/Inventory/WindowActionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinecraftClient.Inventory
+{
+    public enum WindowActionType
+    {
+        LeftClick,
+        RightClick,
+        MiddleClick
+    }
+}
