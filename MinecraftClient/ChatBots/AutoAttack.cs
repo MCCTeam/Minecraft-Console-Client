1529bb51dbdade64a81236195efb0882e8fcaf89
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-05-27 09:25:48 +0500

    Update AutoAttack.cs
    + Fix

--- MinecraftClient/ChatBots/AutoAttack.cs
+++ MinecraftClient/ChatBots/AutoAttack.cs
@@ -46,7 +46,7 @@ namespace MinecraftClient.ChatBots
                         bool shouldAttack = handleEntity(entity.Value);
                         if (shouldAttack)
                         {
-                            SendAnimation(0) // Hit Animation
+                            SendAnimation(0); // Hit Animation
                             InteractEntity(entity.Key, 1); // hit the entity!
                         }
                     }
