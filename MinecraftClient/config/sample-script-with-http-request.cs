1c1d137ab7cef841d1adefcbb83f634b55111944
ORelio <oreliogitantispam.l0gin@spamgourmet.com>
2020-06-07 23:36:56 +0200

    Add example of async HTTP request (#1041)

--- MinecraftClient/config/sample-script-with-http-request.cs
+++ MinecraftClient/config/sample-script-with-http-request.cs
@@ -15,3 +15,20 @@ string PerformHttpRequest(string uri)
             responseString = reader.ReadToEnd();
     return responseString;
 }
+
+void SendHttpPostAsync(string uri, string text)
+{
+    new Thread(() => {
+        var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(uri);
+        request.ContentType = "text/plain";
+        request.Method = "POST";
+        using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            streamWriter.Write(text);
+        var response = (System.Net.HttpWebResponse)request.GetResponse();
+        string responseString;
+        using (var stream = response.GetResponseStream())
+        using (var reader = new StreamReader(stream))
+            responseString = reader.ReadToEnd();
+        //LogToConsole(responseString);
+    }).Start();
+}
