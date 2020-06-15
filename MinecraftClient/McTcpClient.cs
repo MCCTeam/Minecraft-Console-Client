87fb71950946d1efd2feb6ecaec62d7e49e4c5f2
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-06-15 19:00:37 +0500

    Update McTcpClient.cs
    OnTitle

--- MinecraftClient/McTcpClient.cs
+++ MinecraftClient/McTcpClient.cs
@@ -859,7 +859,26 @@ namespace MinecraftClient
                 }
             }
         }
-
+        
+        public void OnTitle(int action, string titletext, string subtitletext, string actionbartext, int fadein, int stay, int fadeout, string json)
+        {
+            foreach (ChatBot bot in bots.ToArray())
+            {
+                try
+                {
+                    bot.OnTitle(action, titletext, subtitletext, actionbartext, fadein, stay, fadeout, json);
+                }
+                catch (Exception e)
+                {
+                    if (!(e is ThreadAbortException))
+                    {
+                        ConsoleIO.WriteLogLine("OnTitle: Got error from " + bot.ToString() + ": " + e.ToString());
+                    }
+                    else throw; //ThreadAbortException should not be caught
+                }
+            }
+        }
+        
         /// <summary>
         /// Received a connection keep-alive from the server
         /// </summary>
