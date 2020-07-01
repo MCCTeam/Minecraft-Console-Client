17c83e2d6773b2577ed476e28697a5d773d997fe
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-07-01 22:38:36 +0500

    Update ChatBot.cs

--- MinecraftClient/ChatBot.cs
+++ MinecraftClient/ChatBot.cs
@@ -1047,13 +1047,15 @@ namespace MinecraftClient
         }
 
         /// <summary>
-        /// Place block
+        /// Place the block at hand in the Minecraft world
         /// </summary>
-        /// <param name="location">Block location</param>
-        /// <returns></returns>
-        protected bool SendPlaceBlock(Location location, Direction blockFace)
+        /// <param name="location">Location to place block to</param>
+        /// <param name="blockFace">Block face (e.g. Direction.Down when clicking on the block below to place this block)</param>
+        /// <param name="hand">Hand.MainHand or Hand.OffHand</param>
+        /// <returns>TRUE if successfully placed</returns>
+        public bool PlaceBlock(Location location, Direction blockFace, Hand hand = Hand.MainHand)
         {
-            return Handler.PlaceBlock(location, blockFace);
+            return handler.SendPlayerBlockPlacement((int)hand, location, blockFace);
         }
 
         /// <summary>
