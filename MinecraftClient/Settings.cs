cdf0724a3f71ebafd9c42d624f79599d87eb666b
Daenges <57369924+Daenges@users.noreply.github.com>
2020-07-05 21:42:43 +0200

    Add Mail to config file

--- MinecraftClient/Settings.cs
+++ MinecraftClient/Settings.cs
@@ -163,13 +163,16 @@ namespace MinecraftClient
         //Auto Eating
         public static bool AutoEat_Enabled = false;
         public static int AutoEat_hungerThreshold = 6;
+        
+        //Mail
+        public static bool Mail_Enabled = false;
 
         //Custom app variables and Minecraft accounts
         private static readonly Dictionary<string, object> AppVars = new Dictionary<string, object>();
         private static readonly Dictionary<string, KeyValuePair<string, string>> Accounts = new Dictionary<string, KeyValuePair<string, string>>();
         private static readonly Dictionary<string, KeyValuePair<string, ushort>> Servers = new Dictionary<string, KeyValuePair<string, ushort>>();
 
-        private enum ParseMode { Default, Main, AppVars, Proxy, MCSettings, AntiAFK, Hangman, Alerts, ChatLog, AutoRelog, ScriptScheduler, RemoteControl, ChatFormat, AutoRespond, AutoAttack, AutoFishing, AutoEat };
+        private enum ParseMode { Default, Main, AppVars, Proxy, MCSettings, AntiAFK, Hangman, Alerts, ChatLog, AutoRelog, ScriptScheduler, RemoteControl, ChatFormat, AutoRespond, AutoAttack, AutoFishing, AutoEat, Mail};
 
         /// <summary>
         /// Load settings from the give INI file
@@ -213,6 +216,7 @@ namespace MinecraftClient
                                     case "autoattack": pMode = ParseMode.AutoAttack; break;
                                     case "autofishing": pMode = ParseMode.AutoFishing; break;
                                     case "autoeat": pMode = ParseMode.AutoEat; break;
+                                    case "mail": pMode = ParseMode.Mail; break;
                                     default: pMode = ParseMode.Default; break;
                                 }
                             }
@@ -700,7 +704,11 @@ namespace MinecraftClient
                 + "# Inventory Handling NEED to be enabled first\r\n"
                 + "enabled=false\r\n"
                 + "threshold=6\r\n"
-                + "\r\n", Encoding.UTF8);
+                + "\r\n"
+                + "[Mail]\r\n"
+                + "# Let the bot act like a mail plugin\r\n"
+                + "enabled=false\r\n"
+                , Encoding.UTF8);
         }
 
         /// <summary>
