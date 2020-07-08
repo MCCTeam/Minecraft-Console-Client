9ff36b8ee51c837335a8650ca86d518eb5358418
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-07-08 07:00:36 +0500

    Update VkMessager.cs

--- MinecraftClient/config/ChatBots/VkMessager.cs
+++ MinecraftClient/config/ChatBots/VkMessager.cs
@@ -115,7 +115,7 @@ internal class VkLongPoolClient
 
     public void SendMessage(string chatId, string text)
     {
-        CallVkMethod("messages.send", "peer_id=" + chatId + "&message=" + text);
+    	CallVkMethod("messages.send", "peer_id=" + chatId + "&random_id=" + LastTs  + "&message=" + text);
     }
 
     private void StartLongPoolAsync()
@@ -164,7 +164,7 @@ internal class VkLongPoolClient
     }
     private string CallVkMethod(string methodName, string data)
     {
-        var url = String.Format("https://api.vk.com/method/{0}?v=5.80&access_token={1}&{2}", methodName, Token, data);
+        var url = String.Format("https://api.vk.com/method/{0}?v=5.120&access_token={1}&{2}", methodName, Token, data);
         var jsonResult = SenderWebClient.DownloadString(url);
 
         return jsonResult;
