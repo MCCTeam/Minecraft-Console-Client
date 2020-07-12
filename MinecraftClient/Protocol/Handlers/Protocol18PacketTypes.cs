be423b6c20e5cd7fd17ecb35fb67dc650d252452
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-07-12 13:27:51 +0500

    Update Protocol18PacketTypes.cs

--- MinecraftClient/Protocol/Handlers/Protocol18PacketTypes.cs
+++ MinecraftClient/Protocol/Handlers/Protocol18PacketTypes.cs
@@ -293,7 +293,7 @@ namespace MinecraftClient.Protocol.Handlers
                     case 0x58: return PacketIncomingType.EntityProperties;
                     case 0x56: return PacketIncomingType.EntityTeleport;
                     case 0x41: return PacketIncomingType.EntityVelocity;
-                    case 0x42: return PacketIncomingType.EntityEquipment;
+                    case 0x46: return PacketIncomingType.EntityEquipment;
                     case 0x59: return PacketIncomingType.EntityEffect;
                     case 0x4E: return PacketIncomingType.TimeUpdate;
                     case 0x48: return PacketIncomingType.UpdateHealth;
