e9bb68c717316d4bc9c3fa3a796e42417dcbfea0
Dusan Milutinovic <bgteam@live.com>
2020-07-27 12:51:48 +0200

    Added a missing comment which I have removed by accident

--- MinecraftClient/Mapping/EntityType.cs
+++ MinecraftClient/Mapping/EntityType.cs
@@ -1,5 +1,17 @@
 namespace MinecraftClient.Mapping
 {
+    /// <summary>	
+    /// Represents Minecraft Entity Types	
+    /// </summary>	
+    /// <remarks>	
+    /// Generated from registries.json using EntityPaletteGenerator.cs.	
+    /// Typical steps to handle new entity IDs for newer Minecraft versions:	
+    /// 1. Generate registries.json using data reporting on Vanilla Minecraft (https://wiki.vg/Data_Generators)	
+    /// 2. Generate temporary EntityTypeXXX.cs and EntityPaletteXXX.cs using EntityPaletteGenerator.cs	
+    /// 3. Perform a diff with existing versions, add missing entries in EntityType.cs and EntityTypeExtensions.cs	
+    /// 4. If existing entity IDs were not randomized by Mojang, simply add missing entries to the latest existing EntityPaletteXXX.cs	
+    /// 5. If existing entity IDs were randomized, add a new palette as EntityPaletteXXX.cs into the codebase (worst case)	
+    /// </remarks>
     public enum EntityType
     {
         AreaEffectCloud,
