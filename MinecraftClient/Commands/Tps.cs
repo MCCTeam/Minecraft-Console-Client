c5c7ddaea1e58b122214684da96d3f846a1bf5ca
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-08-16 16:14:16 +0800

    Add tps command

---
+++ MinecraftClient/Commands/Tps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinecraftClient.Commands
+{
+    class Tps : Command
+    {
+        public override string CMDName { get { return "tps"; } }
+        public override string CMDDesc { get { return "Display server current tps (tick per second). May not be accurate"; } }
+
+        public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
+        {
+            return "Current tps: " + handler.GetServerTPS();
+        }
+    }
+}
