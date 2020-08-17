236e077e44e74d30ad42f30262b3595419a19c45
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-08-17 17:40:06 +0800

    Add TPS command and improve the calculation of TPS (#1210)
    * Add tps command
    * Improve tps calculation and tps command
    * Remove debug log line
    * Improve if-else statement

---
+++ MinecraftClient/Commands/Tps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinecraftClient.Commands
+{
+    class Tps : Command
+    {
+        public override string CMDName { get { return "tps"; } }
+        public override string CMDDesc { get { return "Display server current tps (tick per second). May not be accurate"; } }
+
+        public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
+        {
+            var tps = Math.Round(handler.GetServerTPS(), 2);
+            string color;
+            if (tps < 10)
+                color = "§c";  // Red
+            else if (tps < 15)
+                color = "§e";  // Yellow
+            else color = "§a"; // Green
+            return "Current tps: " + color + tps;
+        }
+    }
+}
