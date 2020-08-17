b71d2925078cb28aa18d276c26a123b1b98bdc57
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-08-17 13:46:03 +0800

    Improve if-else statement

--- MinecraftClient/Commands/Tps.cs
+++ MinecraftClient/Commands/Tps.cs
@@ -12,13 +12,14 @@ namespace MinecraftClient.Commands
 
         public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
         {
-            var tps = handler.GetServerTPS();
-            string color = "§a"; // Green
-            if (tps <= 15)
-                color = "§e";    // Yellow
-            if (tps <= 10)
-                color = "§c";    // Red
-            return "Current tps: " + color + Math.Round(tps, 2);
+            var tps = Math.Round(handler.GetServerTPS(), 2);
+            string color;
+            if (tps < 10)
+                color = "§c";  // Red
+            else if (tps < 15)
+                color = "§e";  // Yellow
+            else color = "§a"; // Green
+            return "Current tps: " + color + tps;
         }
     }
 }
