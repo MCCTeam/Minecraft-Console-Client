0960fa34a6dcbe261ec477681de499512de7af7e
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-08-19 04:38:11 +0500

    Update EntityPose.cs

--- MinecraftClient/Mapping/EntityPose.cs
+++ MinecraftClient/Mapping/EntityPose.cs
@@ -2,12 +2,12 @@ namespace MinecraftClient.Mapping
 {
     public enum EntityPose
     {
-        STANDING = 0,
-        FALL_FLYING = 1,
-        SLEEPING = 2,
-        SWIMMING = 3,
-        SPIN_ATTACK = 4,
-        SNEAKING = 5,
-        DYING = 6,
+        Standing = 0,
+        Fall_Flying = 1,
+        Sleeping = 2,
+        Swimming = 3,
+        Spin_Attack = 4,
+        Sneaking = 5,
+        Dying = 6,
     }
 }
