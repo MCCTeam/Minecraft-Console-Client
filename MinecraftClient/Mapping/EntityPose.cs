fec7ebf13f678ebf9f7e6cb3f1294609e7bcb2ee
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-08-19 04:15:05 +0500

    Create EntityPose.cs

---
+++ MinecraftClient/Mapping/EntityPose.cs
@@ -0,0 +1,13 @@
+namespace MinecraftClient.Mapping
+{
+    public enum EntityPose
+    {
+        STANDING = 0,
+        FALL_FLYING = 1,
+        SLEEPING = 2,
+        SWIMMING = 3,
+        SPIN_ATTACK = 4,
+        SNEAKING = 5,
+        DYING = 6,
+    }
+}
