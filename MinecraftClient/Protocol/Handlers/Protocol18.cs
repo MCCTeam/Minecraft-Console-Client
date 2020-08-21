5cdf5bdca87385549e9c629b880c508247efc975
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-08-21 06:32:12 +0500

    Crashes Fix

--- MinecraftClient/Protocol/Handlers/Protocol18.cs
+++ MinecraftClient/Protocol/Handlers/Protocol18.cs
@@ -911,7 +911,7 @@ namespace MinecraftClient.Protocol.Handlers
                             int EntityID = dataTypes.ReadNextVarInt(packetData);
                             Dictionary<int, object> metadata = dataTypes.ReadNextMetadata(packetData, itemPalette);
                             int healthField = protocolversion >= MC114Version ? 8 : 7; // Health is field no. 7 in 1.10+ and 8 in 1.14+
-                            if (metadata.ContainsKey(healthField) && metadata[healthField].GetType() == typeof(float))
+                            if (metadata.ContainsKey(healthField) && metadata[healthField] != null && metadata[healthField].GetType() == typeof(float))
                                 handler.OnEntityHealth(EntityID, (float)metadata[healthField]);
                             handler.OnEntityMetadata(EntityID, metadata);
                         }
