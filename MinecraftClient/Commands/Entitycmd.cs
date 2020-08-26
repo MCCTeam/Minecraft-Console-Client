1452f4df2628fd790371292917c4cfd9e890bce8
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2020-08-26 13:08:32 +0500

    Update Entitycmd.cs

--- MinecraftClient/Commands/Entitycmd.cs
+++ MinecraftClient/Commands/Entitycmd.cs
@@ -47,7 +47,8 @@ namespace MinecraftClient.Commands
                                         string customname = entity.CustomName;
                                         EntityPose pose = entity.Pose;
                                         EntityType type = entity.Type;
-                                        customname = customname.Replace("&", "§");
+                                        if (!string.IsNullOrEmpty(customname))
+                                            customname = customname.Replace("&", "§");
 
                                         double distance = Math.Round(entity.Location.Distance(handler.GetCurrentLocation()), 2);
 
@@ -59,41 +60,42 @@ namespace MinecraftClient.Commands
 
                                         string location = String.Format("X:{0}, Y:{1}, Z:{2}", Math.Round(entity.Location.X, 2), Math.Round(entity.Location.Y, 2), Math.Round(entity.Location.Y, 2));
 
-                                        string done = String.Format("Entity: {0}\n[MCC] Type: {1}", id, type);
+                                        string done = String.Format(" Entity: {0}\n[MCC] Type: {1}", id, type);
                                         if (!String.IsNullOrEmpty(nickname))
-                                            done += String.Format("\n[MCC] Nickname: {0}", nickname);
+                                            done += String.Format("\n [MCC] Nickname: {0}", nickname);
                                         else if (!String.IsNullOrEmpty(customname))
-                                            done += String.Format("\n[MCC] CustomName: {0}§8", customname);
+                                            done += String.Format("\n [MCC] CustomName: {0}§8", customname);
                                         if (type == EntityType.Player)
-                                            done += String.Format("\n[MCC] Latency: {0}", latency);
+                                            done += String.Format("\n [MCC] Latency: {0}", latency);
                                         else if (type == EntityType.Item || type == EntityType.ItemFrame || type == Mapping.EntityType.EyeOfEnder || type == Mapping.EntityType.Egg || type == Mapping.EntityType.EnderPearl || type == Mapping.EntityType.Potion || type == Mapping.EntityType.Fireball || type == Mapping.EntityType.FireworkRocket)
                                         {
                                             string displayName = item.DisplayName;
                                             if (String.IsNullOrEmpty(displayName))
-                                                done += String.Format("\n[MCC] Item: {0} x{1}", item.Type, item.Count);
+                                                done += String.Format("\n [MCC] Item: {0} x{1}", item.Type, item.Count);
                                             else
-                                                done += String.Format("\n[MCC] Item: {0} x{1} - {2}§8", item.Type, item.Count, displayName);
+                                                done += String.Format("\n [MCC] Item: {0} x{1} - {2}§8", item.Type, item.Count, displayName);
                                         }
-                                        if (entity.Equipment.Count >= 1 && entity.Equipment.ContainsKey(0) || entity.Equipment.ContainsKey(1) || entity.Equipment.ContainsKey(2) || entity.Equipment.ContainsKey(3) || entity.Equipment.ContainsKey(4) || entity.Equipment.ContainsKey(5))
+
+                                        if (entity.Equipment.Count >= 1 && entity.Equipment != null)
                                         {
-                                            done += String.Format("\n[MCC] Equipment:");
+                                            done += String.Format("\n [MCC] Equipment:");
                                             if (entity.Equipment.ContainsKey(0) && entity.Equipment[0] != null)
-                                                done += String.Format("\n [MCC] MainHand: {0} x{1}", entity.Equipment[0].Type, entity.Equipment[0].Count);
+                                                done += String.Format("\n   [MCC] MainHand: {0} x{1}", entity.Equipment[0].Type, entity.Equipment[0].Count);
                                             if (entity.Equipment.ContainsKey(1) && entity.Equipment[1] != null)
-                                                done += String.Format("\n [MCC] OffHand: {0} x{1}", entity.Equipment[1].Type, entity.Equipment[1].Count);
+                                                done += String.Format("\n   [MCC] OffHand: {0} x{1}", entity.Equipment[1].Type, entity.Equipment[1].Count);
                                             if (entity.Equipment.ContainsKey(5) && entity.Equipment[5] != null)
-                                                done += String.Format("\n [MCC] Helmet: {0} x{1}", entity.Equipment[5].Type, entity.Equipment[5].Count);
+                                                done += String.Format("\n   [MCC] Helmet: {0} x{1}", entity.Equipment[5].Type, entity.Equipment[5].Count);
                                             if (entity.Equipment.ContainsKey(4) && entity.Equipment[4] != null)
-                                                done += String.Format("\n [MCC] Chestplate: {0} x{1}", entity.Equipment[4].Type, entity.Equipment[4].Count);
+                                                done += String.Format("\n   [MCC] Chestplate: {0} x{1}", entity.Equipment[4].Type, entity.Equipment[4].Count);
                                             if (entity.Equipment.ContainsKey(3) && entity.Equipment[3] != null)
-                                                done += String.Format("\n [MCC] Leggings: {0} x{1}", entity.Equipment[3].Type, entity.Equipment[3].Count);
+                                                done += String.Format("\n   [MCC] Leggings: {0} x{1}", entity.Equipment[3].Type, entity.Equipment[3].Count);
                                             if (entity.Equipment.ContainsKey(2) && entity.Equipment[2] != null)
-                                                done += String.Format("\n [MCC] Boots: {0} x{1}", entity.Equipment[2].Type, entity.Equipment[2].Count);
+                                                done += String.Format("\n   [MCC] Boots: {0} x{1}", entity.Equipment[2].Type, entity.Equipment[2].Count);
                                         }
-                                        done += String.Format("\n[MCC] Pose: {0}", pose);
-                                        done += String.Format("\n[MCC] Health: {0}", color + health + "§8");
-                                        done += String.Format("\n[MCC] Distance: {0}", distance);
-                                        done += String.Format("\n[MCC] Location: {0}", location);
+                                        done += String.Format("\n [MCC] Pose: {0}", pose);
+                                        done += String.Format("\n [MCC] Health: {0}", color + health + "§8");
+                                        done += String.Format("\n [MCC] Distance: {0}", distance);
+                                        done += String.Format("\n [MCC] Location: {0}", location);
                                         return done;
                                 }
                             }
@@ -149,13 +151,13 @@ namespace MinecraftClient.Commands
                         string location = String.Format("X:{0}, Y:{1}, Z:{2}", Math.Round(entity2.Value.Location.X, 2), Math.Round(entity2.Value.Location.Y, 2), Math.Round(entity2.Value.Location.Y, 2));
 
                         if (type == EntityType.Item || type == EntityType.ItemFrame || type == Mapping.EntityType.EyeOfEnder || type == Mapping.EntityType.Egg || type == Mapping.EntityType.EnderPearl || type == Mapping.EntityType.Potion || type == Mapping.EntityType.Fireball || type == Mapping.EntityType.FireworkRocket)
-                            response.Add(String.Format("#{0}: Type: {1}, Item: {2}, Location: {3}", id, type, entity2.Value.Item.Type, location));
+                            response.Add(String.Format(" #{0}: Type: {1}, Item: {2}, Location: {3}", id, type, entity2.Value.Item.Type, location));
                         else if (type == Mapping.EntityType.Player && !String.IsNullOrEmpty(nickname))
-                            response.Add(String.Format("#{0}: Type: {1}, Nickname: §8{2}§8, Latency: {3}, Health: {4}, Pose: {5}, Location: {6}", id, type, nickname, latency, health, pose, location));
+                            response.Add(String.Format(" #{0}: Type: {1}, Nickname: §8{2}§8, Latency: {3}, Health: {4}, Pose: {5}, Location: {6}", id, type, nickname, latency, health, pose, location));
                         else if (type == Mapping.EntityType.Player && !String.IsNullOrEmpty(customname))
-                            response.Add(String.Format("#{0}: Type: {1}, CustomName: §8{2}§8, Latency: {3}, Health: {4}, Pose: {5}, Location: {6}", id, type, customname.Replace("&", "§"), latency, health, pose, location));
+                            response.Add(String.Format(" #{0}: Type: {1}, CustomName: §8{2}§8, Latency: {3}, Health: {4}, Pose: {5}, Location: {6}", id, type, customname.Replace("&", "§"), latency, health, pose, location));
                         else
-                            response.Add(String.Format("#{0}: Type: {1}, Health: {2}, Location: {3}", id, type, health, location));
+                            response.Add(String.Format(" #{0}: Type: {1}, Health: {2}, Location: {3}", id, type, health, location));
                     }
                     response.Add(CMDDesc);
                     return String.Join("\n", response);
