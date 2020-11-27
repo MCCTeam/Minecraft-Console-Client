0551354d4463507b4f0982b125d9b3fdf24426da
ORelio <ORelio@users.noreply.github.com>
2020-11-27 22:16:58 +0100

    Raise timeout to 30 seconds
    Usually TCP timeouts are 30s

--- MinecraftClient/McClient.cs
+++ MinecraftClient/McClient.cs
@@ -206,7 +206,7 @@ namespace MinecraftClient
             {
                 client = ProxyHandler.newTcpClient(host, port);
                 client.ReceiveBufferSize = 1024 * 1024;
-                client.ReceiveTimeout = 10 * 1000; // 10 seconds
+                client.ReceiveTimeout = 30000; // 30 seconds
                 handler = Protocol.ProtocolHandler.GetProtocolHandler(client, protocolversion, forgeInfo, this);
                 Translations.WriteLine("mcc.version_supported");
 
