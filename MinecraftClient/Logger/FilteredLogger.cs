240468ad228a66dd0548a1049fe158a017d6b85f
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2021-03-07 14:23:26 +0800

    Implement log to file logger (#1494)
    * Implement log to file
    
    Logger moved to it's own namespace
    
    * Add lock to log file

---
+++ MinecraftClient/Logger/FilteredLogger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MinecraftClient.Logger
+{
+    public class FilteredLogger : LoggerBase
+    {
+        protected enum FilterChannel { Debug, Chat }
+
+        protected bool ShouldDisplay(FilterChannel channel, string msg)
+        {
+            Regex regexToUse = null;
+            // Convert to bool for XOR later. Whitelist = 0, Blacklist = 1
+            bool filterMode = Settings.FilterMode == Settings.FilterModeEnum.Blacklist ? true : false;
+            switch (channel)
+            {
+                case FilterChannel.Chat: regexToUse = Settings.ChatFilter; break;
+                case FilterChannel.Debug: regexToUse = Settings.DebugFilter; break;
+            }
+            if (regexToUse != null)
+            {
+                // IsMatch and white/blacklist result can be represented using XOR
+                // e.g.  matched(true) ^ blacklist(true) => shouldn't log(false)
+                return regexToUse.IsMatch(msg) ^ filterMode;
+            }
+            else return true;
+        }
+
+        public override void Debug(string msg)
+        {
+            if (DebugEnabled)
+            {
+                if (ShouldDisplay(FilterChannel.Debug, msg))
+                {
+                    Log("§8[DEBUG] " + msg);
+                }
+                // Don't write debug lines here as it could cause a stack overflow
+            }
+        }
+
+        public override void Info(string msg)
+        {
+            if (InfoEnabled)
+                ConsoleIO.WriteLogLine(msg);
+        }
+
+        public override void Warn(string msg)
+        {
+            if (WarnEnabled)
+                Log("§6[WARN] " + msg);
+        }
+
+        public override void Error(string msg)
+        {
+            if (ErrorEnabled)
+                Log("§c[ERROR] " + msg);
+        }
+
+        public override void Chat(string msg)
+        {
+            if (ChatEnabled)
+            {
+                if (ShouldDisplay(FilterChannel.Chat, msg))
+                {
+                    Log(msg);
+                }
+                else Debug("[Logger] One Chat message filtered: " + msg);
+            }
+        }
+    }
+}
--- MinecraftClient/Protocol/IMinecraftComHandler.cs
+++ MinecraftClient/Protocol/IMinecraftComHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using MinecraftClient.Mapping;
 using MinecraftClient.Inventory;
+using MinecraftClient.Logger;
 
 namespace MinecraftClient.Protocol
 {
