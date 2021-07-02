594cf2248c5ec23fdd10acd49c5e46249f6b3d81
breadbyte <breadbytes@platea.moe>
2021-07-02 16:06:40 +0800

    Replace x86 builds with AnyCPU, Add XUnit Project

---
+++ MinecraftClientTestProject/DNSTest.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace MinecraftClientTestProject {
+    public class DNSTest {
+        [Fact]
+        public void SRVQueryResolve() {
+            
+        }
+    }
+}
