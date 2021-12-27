998b31bcfa4b4f09d9bda4a41ab303d470ed1f39
Daenges <57369924+Daenges@users.noreply.github.com>
2021-12-27 22:19:50 +0100

    Add "-f" to the /move command
    As a solution for #1872 I have implemented a `-f` argument, which forces the movement regardless of its potential consequences.

--- MinecraftClient/Commands/Move.cs
+++ MinecraftClient/Commands/Move.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using MinecraftClient.Mapping;
 
 namespace MinecraftClient.Commands
@@ -14,25 +13,24 @@ namespace MinecraftClient.Commands
 
         public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
         {
-            string[] args = getArgs(command);
-            string argStr = getArg(command).Trim().ToLower();
+            string[] args = getArgs(command.ToLower());
 
-            if (argStr == "on")
+            if (args[0] == "on")
             {
                 handler.SetTerrainEnabled(true);
                 return Translations.Get("cmd.move.enable");
             }
-            else if (argStr == "off")
+            else if (args[0] == "off")
             {
                 handler.SetTerrainEnabled(false);
                 return Translations.Get("cmd.move.disable");
             }
             else if (handler.GetTerrainEnabled())
             {
-                if (args.Length == 1)
+                if (args.Length == 1 || (args.Length == 2 && args.Contains("-f")))
                 {
                     Direction direction;
-                    switch (argStr)
+                    switch (args[0])
                     {
                         case "up": direction = Direction.Up; break;
                         case "down": direction = Direction.Down; break;
@@ -41,12 +39,16 @@ namespace MinecraftClient.Commands
                         case "north": direction = Direction.North; break;
                         case "south": direction = Direction.South; break;
                         case "get": return handler.GetCurrentLocation().ToString();
-                        default: return Translations.Get("cmd.look.unknown", argStr);
+                        default: return Translations.Get("cmd.look.unknown", args[0]);
                     }
                     if (Movement.CanMove(handler.GetWorld(), handler.GetCurrentLocation(), direction))
                     {
-                        handler.MoveTo(Movement.Move(handler.GetCurrentLocation(), direction));
-                        return Translations.Get("cmd.move.moving", argStr);
+                        if (args.Contains("-f"))
+                            handler.MoveTo(Movement.Move(handler.GetCurrentLocation(), direction), allowUnsafe: true);
+                        else
+                            handler.MoveTo(Movement.Move(handler.GetCurrentLocation(), direction));
+
+                        return Translations.Get("cmd.move.moving", args[0]);
                     }
                     else return Translations.Get("cmd.move.dir_fail");
                 }
