0e2ebf484b6b8446acf90b357bab8acac338454f
ORelio <oreliogitantispam.l0gin@spamgourmet.com>
2022-01-26 13:23:18 +0100

    Docker: Update start-latest.sh (grep -i)
    Use -i if case changes for the Location: header

--- Docker/start-latest.sh
+++ Docker/start-latest.sh
@@ -3,7 +3,7 @@
 cd /opt/data || exit 1
 
 # Get latest release tag
-RELEASE_TAG=$(curl -s -v https://github.com/MCCTeam/Minecraft-Console-Client/releases/latest 2>&1 | grep location: | cut -d/ -f8)
+RELEASE_TAG=$(curl -s -v https://github.com/MCCTeam/Minecraft-Console-Client/releases/latest 2>&1 | grep -i location: | cut -d/ -f8)
 
 # Download latest version
 curl -L https://github.com/MCCTeam/Minecraft-Console-Client/releases/download/${RELEASE_TAG}/MinecraftClient.exe --output MinecraftClient.exe
