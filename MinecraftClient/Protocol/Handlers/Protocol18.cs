c362e5b137b41f0765a47d919965d6f57747d84f
Leon Heuer <leon.heuer@outlook.de>
2022-03-21 23:29:16 +0100

    Update version to 1.18.2 (#1961)

--- MinecraftClient/Protocol/Handlers/Protocol18.cs
+++ MinecraftClient/Protocol/Handlers/Protocol18.cs
@@ -53,6 +53,7 @@ namespace MinecraftClient.Protocol.Handlers
         internal const int MC117Version = 755;
         internal const int MC1171Version = 756;
         internal const int MC1181Version = 757;
+        internal const int MC1182Version = 758;
 
         private int compression_treshold = 0;
         private bool autocomplete_received = false;
