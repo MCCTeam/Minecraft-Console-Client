6b3e2a872fb83dc86f9e1423fcb79e6aa7f228f5
Daenges <daenges2@gmail.com>
2022-04-21 01:16:01 +0200

    Add funtion to approach a block as close as possible

--- MinecraftClient/McClient.cs
+++ MinecraftClient/McClient.cs
@@ -216,7 +216,7 @@ namespace MinecraftClient
                     if (Settings.ReplayMod_Enabled) { BotLoad(new ReplayCapture(Settings.ReplayMod_BackupInterval)); }
 
                     //Add your ChatBot here by uncommenting and adapting
-                    BotLoad(new ChatBots.TestBot());
+                    //BotLoad(new ChatBots.YourBot());
                 }
             }
 
