f3b5c972f688cc1ac620b472b95e02cbd909ed8c
BruceChen <MrChen131217@gmail.com>
2022-07-25 18:11:10 +0800

    "/move X Y Z" now moves the player to the center of the block first

--- MinecraftClient/Mapping/Location.cs
+++ MinecraftClient/Mapping/Location.cs
@@ -307,7 +307,7 @@ namespace MinecraftClient.Mapping
         /// <returns>String representation of the location</returns>
         public override string ToString()
         {
-            return String.Format("X:{0} Y:{1} Z:{2}", X, Y, Z);
+            return String.Format("X:{0:0.00} Y:{1:0.00} Z:{2:0.00}", X, Y, Z);
         }
     }
 }
