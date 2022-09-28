85e1458e67ac37c086df3a1881e2dc3475e613ed
Milutinke <bgteam@live.com>
2022-09-28 21:39:13 +0200

    Leave bed command.

---
+++ MinecraftClient/Commands/Leavebed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MinecraftClient.Commands
+{
+    public class leaveBedCommand : Command
+    {
+        public override string CmdName { get { return "leavebed"; } }
+        public override string CmdUsage { get { return "leavebed"; } }
+        public override string CmdDesc { get { return "cmd.leavebed.desc"; } }
+
+        public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
+        {
+            handler.SendEntityAction(Protocol.EntityActionType.LeaveBed);
+            return Translations.TryGet("cmd.leavebed.leaving");
+        }
+    }
+}
