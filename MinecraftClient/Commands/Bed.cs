ff7021e79872b729f645706bde5617874dbfeb0f
BruceChen <MrChen131217@gmail.com>
2022-09-30 08:47:02 +0800

    Upgrade old coordinate parsing.

--- MinecraftClient/Commands/Bed.cs
+++ MinecraftClient/Commands/Bed.cs
@@ -121,12 +121,8 @@ namespace MinecraftClient.Commands
 
                     if (args.Length >= 3)
                     {
-                        Location current = handler.GetCurrentLocation();
-                        double x = args[1].StartsWith('~') ? current.X + (args[1].Length > 1 ? double.Parse(args[1][1..]) : 0) : double.Parse(args[1]);
-                        double y = args[2].StartsWith('~') ? current.Y + (args[2].Length > 1 ? double.Parse(args[2][1..]) : 0) : double.Parse(args[2]);
-                        double z = args[3].StartsWith('~') ? current.Z + (args[3].Length > 1 ? double.Parse(args[3][1..]) : 0) : double.Parse(args[3]);
-
-                        Location block = new Location(x, y, z).ToFloor(), blockCenter = block.ToCenter();
+                        Location block = Location.Parse(handler.GetCurrentLocation(), args[1], args[2], args[3]).ToFloor();
+                        Location blockCenter = block.ToCenter();
 
                         if (!handler.GetWorld().GetBlock(block).Type.IsBed())
                             return Translations.TryGet("cmd.bed.not_a_bed", blockCenter.X, blockCenter.Y, blockCenter.Z);
