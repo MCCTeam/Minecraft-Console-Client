c1a04fe5bf8b0033713d825aa28b63b73586c4e5
BruceChen <MrChen131217@gmail.com>
2022-10-07 21:49:05 +0800

    Bug fix

--- MinecraftClient/Program.cs
+++ MinecraftClient/Program.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
