e5529eead94b9b9a042f8f2d0331c13031260695
BruceChen <MrChen131217@gmail.com>
2022-11-02 21:01:32 +0800

    Migrate documents

---
+++ docs/.vuepress/configs/head.ts
@@ -0,0 +1,8 @@
+import type { HeadConfig } from '@vuepress/core'
+
+export const head: HeadConfig[] = [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+    ["link", { rel: "icon", href: "../../favicon.ico" }],
+]
