bb160d6d84eedb2bf69442d9f541089ac676b440
Milutinke <bgteam@live.com>
2022-12-21 12:51:27 +0100

    Server Data and Profileless Chat message packets implemented. ChatPreview packet removed.  Message Header packet set only to 1.19.2.  Chat Message packet partially implemented.

---
+++ MinecraftClient/Mapping/MessageFilterType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftClient.Mapping
+{
+    public enum MessageFilterType
+    {
+        PassThrough = 0,
+        FullyFiltered,
+        PartiallyFiltered
+    }
+}
