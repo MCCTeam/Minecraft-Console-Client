c475ee9cc9c88f149a0d68eed159be0c01d5bc03
oldkingOK <oldkingok@qq.com>
2024-01-14 10:48:45 +0800

    Remove QQbot scripts for fml3 pull request

--- MinecraftClient/McClient.cs
+++ MinecraftClient/McClient.cs
@@ -7,7 +7,6 @@ using System.Threading;
 using Brigadier.NET;
 using Brigadier.NET.Exceptions;
 using MinecraftClient.ChatBots;
-using MinecraftClient.OkBots;
 using MinecraftClient.CommandHandler;
 using MinecraftClient.CommandHandler.Patch;
 using MinecraftClient.Commands;
@@ -299,7 +298,6 @@ namespace MinecraftClient
             if (Config.ChatBot.TelegramBridge.Enabled) { BotLoad(new TelegramBridge()); }
             if (Config.ChatBot.ItemsCollector.Enabled) { BotLoad(new ItemsCollector()); }
             if (Config.ChatBot.WebSocketBot.Enabled) { BotLoad(new WebSocketBot()); }
-            if (Config.ChatBot.OkWsBot.Enabled) { BotLoad(new OkWsBot()); }
             //Add your ChatBot here by uncommenting and adapting
             //BotLoad(new ChatBots.YourBot());
         }
