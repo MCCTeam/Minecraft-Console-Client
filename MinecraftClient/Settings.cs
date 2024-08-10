efe23eb1f95a94ea87aa857688c14bc24b0cbdec
a08381 <632785425@163.com>
2024-08-11 01:43:46 +0800

    add Yggdrasil authlib multi-user selection.

--- MinecraftClient/Settings.cs
+++ MinecraftClient/Settings.cs
@@ -496,6 +496,9 @@ namespace MinecraftClient
                     public LoginMethod Method = LoginMethod.mcc;
                     [TomlInlineComment("$Main.General.AuthlibServer$")]
                     public AuthlibServer AuthServer = new(string.Empty);
+
+                    [TomlInlineComment("$Main.General.AuthlibUser$")]
+                    public string AuthUser = "";
                   
 
                     public enum LoginType { mojang, microsoft,yggdrasil };
